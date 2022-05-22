b323cda112d58994d32be517db702368aff5825f
dikiharyadi19 <dk.harryadi@gmail.com>
2022-05-22 10:42:35 +0700

    adding address repo

--- repository/address/address_repository.go
+++ repository/address/address_repository.go
@@ -1,1 +1,74 @@
-package repository
+package address
+
+import (
+	"infolelang/lib"
+	models "infolelang/models/addresses"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type AddressDefinition interface {
+	GetAll() (responses []models.AddressesResponse, err error)
+	GetOne(id int64) (responses models.AddressesResponse, err error)
+	Store(request *models.AddressesRequest) (responses bool, err error)
+	Update(request *models.AddressesRequest) (responses bool, err error)
+	Delete(id int64) (err error)
+	WithTrx(trxHandle *gorm.DB) AddressRepository
+}
+type AddressRepository struct {
+	db      lib.Database
+	db2     lib.Databases
+	elastic lib.Elasticsearch
+	logger  lib.Logger
+	timeout time.Duration
+}
+
+func NewAddressReporitory(
+	db lib.Database,
+	db2 lib.Databases,
+	elastic lib.Elasticsearch,
+	logger lib.Logger) AddressDefinition {
+	return AddressRepository{
+		db:      db,
+		db2:     db2,
+		elastic: elastic,
+		logger:  logger,
+		timeout: time.Second * 100,
+	}
+}
+
+// WithTrx implements AddressDefinition
+func (address AddressRepository) WithTrx(trxHandle *gorm.DB) AddressRepository {
+	if trxHandle == nil {
+		address.logger.Zap.Error("transaction Database not found in gin context. ")
+		return address
+	}
+	address.db.DB = trxHandle
+	return address
+}
+
+// GetAll implements AddressDefinition
+func (address AddressRepository) GetAll() (responses []models.AddressesResponse, err error) {
+	return responses, address.db.DB.Find(&responses).Error
+}
+
+// GetOne implements AddressDefinition
+func (address AddressRepository) GetOne(id int64) (responses models.AddressesResponse, err error) {
+	return responses, address.db.DB.Where("id = ?", id).Find(&responses).Error
+}
+
+// Store implements AddressDefinition
+func (address AddressRepository) Store(request *models.AddressesRequest) (responses bool, err error) {
+	return responses, address.db.DB.Save(&responses).Error
+}
+
+// Update implements AddressDefinition
+func (address AddressRepository) Update(request *models.AddressesRequest) (responses bool, err error) {
+	return true, address.db.DB.Save(&responses).Error
+}
+
+// Delete implements AddressDefinition
+func (address AddressRepository) Delete(id int64) (err error) {
+	return address.db.DB.Where("id = ?", id).Delete(&models.AddressesResponse{}).Error
+}
