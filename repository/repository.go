2ba7bd16bf2bd54de513d7e8387d693d40dd1c7f
dikiharyadi19 <dk.harryadi@gmail.com>
2022-06-08 09:23:16 +0700

    adding some repository

--- repository/repository.go
+++ repository/repository.go
@@ -2,9 +2,13 @@ package repository
 
 import (
 	ap "infolelang/repository/access_places"
+	address "infolelang/repository/address"
+	approval "infolelang/repository/approvals"
 	asset "infolelang/repository/asset"
 	category "infolelang/repository/categories"
+	contact "infolelang/repository/contacts"
 	faq "infolelang/repository/faq"
+	image "infolelang/repository/images"
 	kpknl "infolelang/repository/kpknl"
 	subCategory "infolelang/repository/sub_categories"
 	user "infolelang/repository/user"
@@ -19,7 +23,16 @@ var Module = fx.Options(
 	fx.Provide(ap.NewAccessPlaceReporitory),
 	fx.Provide(faq.NewFaqReporitory),
 	fx.Provide(asset.NewAssetReporitory),
+	fx.Provide(asset.NewBuildingAssetReporitory),
+	fx.Provide(asset.NewVehicleAssetReporitory),
+	fx.Provide(asset.NewAssetFacilityReporitory),
+	fx.Provide(asset.NewAssetAccessPlaceReporitory),
+	fx.Provide(asset.NewAssetImageReporitory),
 	fx.Provide(category.NewCategoryReporitory),
 	fx.Provide(kpknl.NewKpknlReporitory),
 	fx.Provide(subCategory.NewSubCategoryReporitory),
+	fx.Provide(address.NewAddressReporitory),
+	fx.Provide(approval.NewApprovalReporitory),
+	fx.Provide(contact.NewContactReporitory),
+	fx.Provide(image.NewImageReporitory),
 )
